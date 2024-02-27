12a9cee2d6bac4ef6349d9ae2bd5e3d10f553436
Maje97 <maje@hotmail.se>
2024-02-27 21:33:30 +0100

    fixing mobile nav
    Fixat mobilmeny

--- interactive.js
+++ interactive.js
@@ -1,10 +1,18 @@
 const droplinks = document.querySelector('.droplinks');
+let x = 1;
 
 function mobileMenu () {
-    droplinks.innerHTML =  `<a href="index.html" target="_self">Hem</a>
-    <a href="stensaxpase.html" target="_self">Sten Sax Påse</a>
-    <a href="adoptapet.html" target="_self">Kattspel</a>
-    <a href="iframesvideo.html" target="_self">Videospelare</a>`;
+    x++;
+
+    if (x == 1) {
+        droplinks.innerHTML =  `<a href="index.html" target="_self">Hem</a>
+        <a href="stensaxpase.html" target="_self">Sten Sax Påse</a>
+        <a href="adoptapet.html" target="_self">Kattspel</a>
+        <a href="iframesvideo.html" target="_self">Videospelare</a>`;
+    } else {
+        droplinks.innerHTML = '';
+        x = 0;
+    }
 }
 
 function eraseMenu () {
